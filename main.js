a0e7d02f68afb2c7921bc34bdf8cdd0035a8b5d4
Neo-Machina <andreistatache1997@gmail.com>
2022-06-20 17:32:07 +0200

    milestone 2 completed

--- main.js
+++ main.js
@@ -12,12 +12,13 @@
 // Milestone 2 - Prendendo come riferimento il layout di esempio presente nell'html, stampiamo i post del nostro feed.
 // Milestone 3 - Se clicchiamo sul tasto "Mi Piace" cambiamo il colore al testo del bottone e incrementiamo il counter dei likes relativo.
 
+// MILESTONE 1
 // Creo un array di oggetti per rappresentare ciascun post
-const userPostArray = [
+const usersPost = [
     {
         id: 1,
         name: 'Tommaso Bianchi',
-        picture: 'https://unsplash.it/300/300?image=1',
+        profilePic: 'https://unsplash.it/300/300?image=1',
         date: '12/25/2020',
         text: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias',
         image: 'https://unsplash.it/300/300?image=10',
@@ -26,7 +27,7 @@ const userPostArray = [
     {
         id: 2,
         name: 'Andrea Rossi',
-        picture: 'https://unsplash.it/300/300?image=1',
+        profilePic: 'https://unsplash.it/300/300?image=2',
         date: '06/02/2021',
         text: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias',
         image: 'https://unsplash.it/300/300?image=13',
@@ -35,7 +36,7 @@ const userPostArray = [
     {
         id: 3,
         name: 'Giulia Gialli',
-        picture: 'https://unsplash.it/300/300?image=1',
+        profilePic: 'https://unsplash.it/300/300?image=8',
         date: '08/08/2019',
         text: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias',
         image: null,
@@ -43,4 +44,62 @@ const userPostArray = [
     }
 ];
 
-console.log(userPostArray);
+drawAllUsersPost(usersPost);
+
+// MILESTONE 2
+// Per ogni elemeno di usersPost lo stampo nel DOM
+// usersPostArray -> array di post di utenti
+function drawAllUsersPost(usersPostArray) {
+    // Creo costante del container dove incorporo i singoli post
+    const postsContainer = document.querySelector('#container');
+    
+    // Scrollo ogni singolo elemento dell'array 
+    for(let i = 0; i < usersPostArray.length; i++) {
+        const singleUserPost = usersPostArray[i];
+        const {id, name, profilePic, date, text, image, likes} = singleUserPost;
+        
+        // Per ogni post creo il template e lo stampo
+        const userPostTemplate =`
+        <div class="post">
+            <div class="post__header">
+                <div class="post-meta">                    
+                    <div class="post-meta__icon">
+                        <img class="profile-pic" src="${profilePic}" alt="Phil Mangione">                    
+                    </div>
+                    <div class="post-meta__data">
+                        <div class="post-meta__author">${name}</div>
+                        <div class="post-meta__time">${date}</div>
+                    </div>                    
+                </div>
+            </div>
+            <div class="post__text">${text}</div>
+            ${image === null ? '' : getImagePostHtml (image)}
+            <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <a class="like-button  js-like-button" href="#" data-postid="${id}">
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                            <span class="like-button__label">Mi Piace</span>
+                        </a>
+                    </div>
+                    <div class="likes__counter">
+                        Piace a <b id="like-counter-1" class="js-likes-counter">${likes}</b> persone
+                    </div>
+                </div> 
+            </div>            
+        </div>
+        `;
+
+        // Aggiungo ogni userPostTemplate a postsContainer
+        postsContainer.innerHTML += userPostTemplate;
+    }
+}
+
+
+function getImagePostHtml (imagePost) {
+    return `
+    <div class="post__image">
+        <img src="${imagePost}" alt="image">
+    </div>
+    `;
+}
